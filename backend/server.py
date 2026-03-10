8a378a887e50c9fbfaf2a13467929871aa0b650f
Dellano <dellanog0@gmail.com>
2026-03-10 15:08:31 +0000

    fix: add authSource admin to MongoDB connection

--- backend/server.py
+++ backend/server.py
@@ -24,7 +24,7 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(mongo_url, authSource="admin")
 db = client[os.environ['DB_NAME']]
 
 # Create the main app without a prefix
